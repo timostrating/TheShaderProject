8f43f39ac56f76a6558ec6c1566f25b1f0742509
Timo Strating <timostrating@home.nl>
2017-06-16 23:44:56 +0200

    next schader

---
+++ TheShaderProject/Assets/CODE/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+    [SerializeField]
+    Vector3 rotation;
+    Transform myTransform;
+
+
+    void Start() {
+        myTransform = transform;
+    }
+
+    void Update () {
+		myTransform.Rotate(rotation);
+	}
+}
